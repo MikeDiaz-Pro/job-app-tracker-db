f94c8d992f3eb777c4e45b838cbbcc34fc364ff4
Mike Pro <mtechnetwork24@gmail.com>
2025-09-02 17:52:39 -0600

    chore(docker): update docker-compose for Postgres with restart policy
    - Added `restart: always` to Postgres service for auto-recovery
    - Ensures container restarts on failure or system reboot
    - Improves local development reliability

---
+++ api/job-app-tracker-api/src/test/java/com/mikediazpro/jat/JobAppTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikediazpro.jat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
